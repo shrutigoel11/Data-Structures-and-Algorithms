60ca759c039cb361f6fa5a172747b233e54c17af
shrutigoel11 <shrutigoel0111@gmail.com>
2024-06-27 17:42:07 +0530

    27-june-update

---
+++ Queue/Questions/First non-repeating character/solution.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <queue>
+using namespace std;
+string FirstNonRepeating(string A){
+		    map<char, int> m;
+		    string ans = "";
+		    queue<char> q;
+		    
+		    for(int i=0; i<A.length(); i++) {
+		        char ch = A[i];
+		        
+		        q.push(ch);
+		        m[ch]++;
+		        
+		        while(!q.empty()) {
+		            if(m[q.front()] > 1){
+		                q.pop();
+		            }
+		            else
+		            {
+		                ans.push_back(q.front());
+		                break;
+		            }
+		        }
+		        if(q.empty()){
+		            ans.push_back('#');
+		        }
+		    }
+		    return ans;
+		}
+
+int main()
+{
+
+
+
+return 0;
+}
