f42a994de700a1fa0e19d43d246ada05a1753dfb
shrutigoel11 <shrutigoel0111@gmail.com>
2024-07-06 22:24:48 +0530

    added new files

--- DSA Course 2/Lecture-3/tempCodeRunnerFile.cpp
+++ DSA Course 2/Lecture-3/tempCodeRunnerFile.cpp
@@ -1,1 +1,51 @@
-for()
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+//upside
+  //star
+for(int i =0 ;i< n ;i++){
+    for(int j=0;j<=n-i-1;j++){
+        cout<<"*";
+    }
+    //space
+    for(int j=0;j<=2*i;j++){
+    cout<<" ";
+   }
+   //star
+   for(int j=0;j<n-i;j++){
+    cout<<"*";
+   }cout<<endl;
+}
+//downside
+        //star
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++){
+        cout<<"*";
+    }
+       //space
+       for(int j=0;j<=2*(n-i-1);j++){
+        cout<<" ";
+       }
+       //star
+       for(int j=0;j<=i;j++){
+        cout<<"*";
+       }cout<<endl;
+
+    }
+   
+
+}
+
+int main()
+{
+int n;
+cout<<"Enter n: ";
+cin>>n;
+print(n);
+
+
+return 0;
+}
