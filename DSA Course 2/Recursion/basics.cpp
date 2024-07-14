2c3f9fd5abbedf0b12de62e51ca6e284a38d00af
shrutigoel11 <shrutigoel0111@gmail.com>
2024-07-14 21:15:29 +0530

    updated files

--- DSA Course 2/Recursion/basics.cpp
+++ DSA Course 2/Recursion/basics.cpp
@@ -1,1 +1,20 @@
-//Recursion -> doing one task once and repeating it by its own.
+//Recursion -> Function calling itself until a specific condition is met.
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void f(int &count){
+
+    if(count==3) return; //statements in this call will not be executed and terminated and goes back to previous call it repeats till all calls are terminated
+    cout<<count<<endl;
+    count++;
+    f(count);
+}
+
+int main()
+{
+int count=0;
+f(count);
+
+return 0;
+}
